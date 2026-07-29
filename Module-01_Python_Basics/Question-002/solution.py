fdb922aec5d3825a32081d9e44293dfb87016615
Anagha K <anagharajesh131@gmail.com>
2026-07-29 15:49:07 +0530

    Implement resistance calculation based on inputs

---
+++ Module-01_Python_Basics/Question-002/solution.py
@@ -0,0 +1,5 @@
+d1=int(input("Enter first digit="))
+d2=int(input("Enter second digit="))
+multiplier=int(input("enter multiplier"))
+resistance=(10*d1+d2)*10**multiplier
+print(f"Resistance ={resistance} ohms")
